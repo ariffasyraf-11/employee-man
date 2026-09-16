58ca24de95eb728bc6b3001a66621911eb9aa694
ariffasyraf-11 <ariffasyraf.0321@gmail.com>
2026-09-16 13:07:20 +0800

    chore: add Capacitor configuration

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.employeeman',
+  appName: 'Employee Man',
+  webDir: 'dist/employee-man/browser'
+};
+
+export default config;
